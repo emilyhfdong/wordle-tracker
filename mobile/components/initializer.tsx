fabdc835a0f936425ae56e2141f6128d892c68d4
Emily Dong <emilyhfdong@gmail.com>
2022-03-30 12:50:11 -0400

    feat: handle in app notifications

--- mobile/components/initializer.tsx
+++ mobile/components/initializer.tsx
@@ -8,6 +8,14 @@ import { FullScreenLoading } from "./full-screen-loading"
 import * as Device from "expo-device"
 import * as Notifications from "expo-notifications"
 
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
+})
+
 export const Initializer: React.FC = ({ children }) => {
   const userId = useAppSelector((state) => state.user.id)
 
